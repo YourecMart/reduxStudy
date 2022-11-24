2674483ddbd01f2dfa0431fcec2b417f48d16e77
Yourec <Rulet.puzyaka@yandex.ru>
2022-11-24 15:07:41 +0300

    reduxStudy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
+
+const dispatch = useDispatch()
+
+const cash = useSelector(state => state.cash)
+
+console.log(cash)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'flex' }}>
+        <button>Пополнить счет</button>
+        <button>Снять со счета</button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const defaultState = {
+    cash: 0,
+}
+
+
+
+const bankReducer = (state = defaultState, action) => {
+
+    switch (action.type) {
+
+        case 'ADD_CASH':
+            return { ...state, cash: state.cash + action.payload }
+
+        case 'GET_CASH':
+            return { ...state, cash: state.cash - action.payload }
+
+        default:
+            return state
+    }
+
+
+
+}
+
+
+
+
+const store = configureStore(bankReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
+
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
